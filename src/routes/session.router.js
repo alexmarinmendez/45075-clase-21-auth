e4dadca5f26cdad4e4714af82c6850f3d28711d2
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-22 18:47:24 -0500

    feat: add bcrypt

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import UserModel from "../dao/models/user.model.js";
+import { createHash, isValidPassword } from "../utils.js";
 
 const router = Router()
 
@@ -10,7 +11,13 @@ router.get('/register', (req, res) => {
 
 // API para crear usuarios en la DB
 router.post('/register', async(req, res) => {
-    const userNew = req.body
+    const userNew = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        age: req.body.age,
+        email: req.body.email,
+        password: createHash(req.body.password)
+    }
     console.log(userNew);
 
     const user = new UserModel(userNew)
@@ -28,13 +35,16 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body
 
-    const user = await UserModel.findOne({email, password}).lean().exec()
+    const user = await UserModel.findOne({email}).lean().exec()
     if(!user) {
         return res.status(401).render('errors/base', {
-            error: 'Error en email y/o password'
+            error: 'User not found'
         })
     }
-
+    if (!isValidPassword(user, password)) {
+        return res.status(403).send({ status: "error", error: "Incorrect password"})
+    }
+    delete user.password
     req.session.user = user
     res.redirect('/products')
 })
