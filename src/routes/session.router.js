607127165bce2439bf79a8f1da314e20c56df3ab
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-22 19:39:51 -0500

    feat: add passport local

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UserModel from "../dao/models/user.model.js";
 import { createHash, isValidPassword } from "../utils.js";
+import passport from 'passport'
 
 const router = Router()
 
@@ -10,20 +11,13 @@ router.get('/register', (req, res) => {
 })
 
 // API para crear usuarios en la DB
-router.post('/register', async(req, res) => {
-    const userNew = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        age: req.body.age,
-        email: req.body.email,
-        password: createHash(req.body.password)
-    }
-    console.log(userNew);
-
-    const user = new UserModel(userNew)
-    await user.save()
-
+router.post('/register', passport.authenticate('register', {failureRedirect: '/session/failRegister'}), async(req, res) => {
+    console.log('testing...')
     res.redirect('/session/login')
+})
+
+router.get('/failRegister', (req, res) => {
+    res.send({ error: 'failRegister'})
 })
 
 // Vista de Login
@@ -32,21 +26,23 @@ router.get('/login', (req, res) => {
 })
 
 // API para login
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body
+router.post('/login', passport.authenticate('login', { failureRedirect: '/session/failLogin'}), async (req, res) => {
 
-    const user = await UserModel.findOne({email}).lean().exec()
-    if(!user) {
-        return res.status(401).render('errors/base', {
-            error: 'User not found'
-        })
+    if (!req.user) {
+        return res.status(400).send({ status: 'error', error: 'Invalid credentiales' })
     }
-    if (!isValidPassword(user, password)) {
-        return res.status(403).send({ status: "error", error: "Incorrect password"})
+
+    req.session.user = {
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+        email: req.user.email,
+        age: req.user.age
     }
-    delete user.password
-    req.session.user = user
     res.redirect('/products')
+})
+
+router.get('/failLogin', (req, res) => {
+    res.send({ error: 'Fail Login' })
 })
 
 // Cerrar Session
@@ -55,7 +51,7 @@ router.get('/logout', (req, res) => {
         if(err) {
             console.log(err);
             res.status(500).render('errors/base', {error: err})
-        } else res.redirect('/sessions/login')
+        } else res.redirect('/session/login')
     })
 })
 
